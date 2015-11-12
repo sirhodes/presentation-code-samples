88dd49efd622faa6b3843a541149575abc44d246
Seth Rhodes <seth@thinkpixbit.com>
2015-11-12 09:36:37 -0500

    greetUser example

---
+++ greetUser.js
@@ -0,0 +1,31 @@
+var os = require('os');
+var NODE_VERSION = process.version;
+var MACHINE = outputMachine(process.platform);
+var USER = process.env.USER || os.hostname();
+console.log(
+  'Hello %s running Node %s on a %s machine!',
+  USER,
+  NODE_VERSION,
+  MACHINE
+);
+
+function outputMachine(platform) {
+  var machine = null;
+  switch (true) {
+    case platform === 'darwin':
+      machine = 'Mac';
+      break;
+    case platform === 'win32':
+      machine = 'Windows';
+      break;
+    case platform === 'sunos':
+    case platform === 'freebsd':
+      machine = 'Unix';
+      break;
+    default:
+      machine = 'not recognized, please send a Pull Request!';
+      break;
+  }
+
+  return machine;
+}
