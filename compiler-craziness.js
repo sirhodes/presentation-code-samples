5172c02e9e8f9dcdf87f87706b57e05fb55e28da
Seth Rhodes <seth@thinkpixbit.com>
2015-11-12 10:34:47 -0500

    example of tricking the compiler

---
+++ compiler-craziness.js
@@ -0,0 +1,8 @@
+console.log('2 + 2 \n %s \n', 2 + 2);
+console.log('2 + \'2\' \n %s \n', 2 + '2');
+console.log('2 - \'2\' \n %s \n', 2 - '2');
+console.log('2 - \'a\' \n %s \n', 2 - 'a');
+console.log('2 + \'a\' \n %s \n', 2 + 'a');
+console.log('[] + [] \n %s \n', [] + []);
+console.log('[] + {} \n %s \n', [] + {});
+console.log('{} + {} \n %s \n', {} + {});
